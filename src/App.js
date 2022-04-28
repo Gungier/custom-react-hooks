34c76fa7ddc4e49821219905ca4926d65a0116e1
Gabe <gungier1@gmail.com>
2022-04-28 10:24:06 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Card, CardGrid, Container, Header } from "./Elements";
 import "./App.css";
+import Toggle from "./components/Toggle";
+import Increment from "./components/Increment";
 import Menu from "./Menu";
 import blue from "./blue.png";
 import purp from "./purp.png";
@@ -16,22 +18,24 @@ function App() {
       </Header>
       <Container>
         <h2>Super Cool</h2>
+        <Toggle />
+        <Increment />
         <CardGrid>
           <Card style={{ background: "var(--purp)" }}>
             <h3>Some card</h3>
-            <img src={purp} />
+            <img src={purp} alt="purp" />
           </Card>
           <Card style={{ background: "var(--blue)" }}>
             <h3>Some card</h3>
-            <img src={blue} />
+            <img src={blue} alt="blue" />
           </Card>
           <Card style={{ background: "var(--black)" }}>
             <h3>Some card</h3>
-            <img src={black} />
+            <img src={black} alt="black" />
           </Card>
           <Card style={{ background: "var(--green)" }}>
             <h3>Some card</h3>
-            <img src={green} />
+            <img src={green} alt="greenn" />
           </Card>
         </CardGrid>
       </Container>
---
+++ src/components/Increment.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useInc } from "../hooks/useInc";
+
+const Increment = () => {
+  const [value, { dec, inc }] = useInc();
+  return (
+    <div>
+      <button onClick={dec}>-</button>
+      {value}
+      <button onClick={inc}>+</button>
+    </div>
+  );
+};
+
+export default Increment;
